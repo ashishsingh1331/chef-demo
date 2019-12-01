340f353d21fb2428096fd05d98a244cd81605d06
vagrant <vagrant@vagrant.vm>
2019-12-01 08:14:41 +0000

    setup file added

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,7 @@
 # Cookbook:: chef-workstation
 # Recipe:: default
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+# Copyright:: 2019, The Authors, All Rights Reserved
+
+include_recipe 'chef-workstation::setup'
+
